4300f5f7ebb5e39f4e0016907063ec13fbcc98ec
Mariusz Andrzejewski <mariusz.andrzejewski@droptica.pl>
2023-07-20 08:09:46 +0200

    Fix issue with installing plugin via CLI - mautic:plugins:reload, by removing the db transaction on plugin install.

--- MauticBeefreeBundle.php
+++ MauticBeefreeBundle.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     Mautic
  * @copyright   2020 Enguerr. All rights reserved
@@ -9,8 +10,33 @@
 
 namespace MauticPlugin\MauticBeefreeBundle;
 
+use Doctrine\ORM\Tools\SchemaTool;
+use Mautic\CoreBundle\Factory\MauticFactory;
 use Mautic\PluginBundle\Bundle\PluginBundleBase;
+use Mautic\PluginBundle\Entity\Plugin;
 
 class MauticBeefreeBundle extends PluginBundleBase
 {
+  public static function onPluginInstall(Plugin $plugin, MauticFactory $factory, $metadata = null, $installedSchema = null)
+  {
+    if (null !== $metadata) {
+      static::installPluginSchema($metadata, $factory, $installedSchema);
+    }
+  }
+
+  public static function installPluginSchema(array $metadata, MauticFactory $factory, $installedSchema = null)
+  {
+    if (null !== $installedSchema) {
+      // Schema exists so bail
+      return;
+    }
+
+    $db             = $factory->getDatabase();
+    $schemaTool     = new SchemaTool($factory->getEntityManager());
+    $installQueries = $schemaTool->getCreateSchemaSql($metadata);
+
+    foreach ($installQueries as $q) {
+      $db->query($q);
+    }
+  }
 }
